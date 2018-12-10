025574f8d746c48c86c5cacc1b65bb2a58bd412b
cartridsmith <yapimda@gmail.com>
2018-12-10 17:57:47 +0700

    Menambahkan Refresh

--- app/src/main/java/redlocks/app/portalti16/network/Network.java
+++ app/src/main/java/redlocks/app/portalti16/network/Network.java
@@ -1,14 +1,28 @@
 package redlocks.app.portalti16.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Network {
 
     public static Retrofit request(){
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+
         return new Retrofit.Builder()
                 //ini main url dari web services yg tersedia
                 .baseUrl("http://35.186.145.167:1337/")
+                //tambahkan client okhttp
+                .client(client)
                 //ini melakukan konversi dari json ke java object
                 .addConverterFactory(GsonConverterFactory.create())
                 //build it!
